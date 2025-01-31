5a91a14215eda9d71188c76d0f202c40cb8a6d2e
robertsmith2018 <oakcodes@gmail.com>
2025-01-31 02:48:15 +0200

    Ready to be published to Amazzon

---
+++ build/precache-manifest.20d3ffd77441a598f57784703cb81aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b28e7c13639c63b33bc3",
+    "url": "/static/js/main.d0c0e0a4.chunk.js"
+  },
+  {
+    "revision": "d6551457c70e6463294c",
+    "url": "/static/js/2.09498be9.chunk.js"
+  },
+  {
+    "revision": "b28e7c13639c63b33bc3",
+    "url": "/static/css/main.10c73eda.chunk.css"
+  },
+  {
+    "revision": "2f5258e8cfec29eb1c5491cd5dbad436",
+    "url": "/index.html"
+  }
+];
